61bf9130360fe78ceba3af3623016f32db2bd95f
Carlos Anguiano <carlos.anguiano@gmail.com>
2017-01-19 00:24:58 -0600

    Hola Mundo

--- app/src/main/java/com/helloworld/devix/helloworld/MainActivity.java
+++ app/src/main/java/com/helloworld/devix/helloworld/MainActivity.java
@@ -10,9 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-            //001
-            //002 carlos
-        //1
-        //2
+
     }
 }
